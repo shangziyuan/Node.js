159cd6cdd5526dd9dddf386485e25e0eb83ddb5b
Shang Zi Yuan <happy.red.bear@gmail.com>
2021-05-04 17:31:27 +0800

    Section 1.11: How to get a basic HTTPS web server up and running

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+var httpsOptions = {
+    key: fs.readFileSync('/home/ubuntu/conf/ca-key.pem'),
+    cert: fs.readFileSync('/home/ubuntu/conf/ca-crt.pem')
+};
+
+var app = function (req, res) { 
+    res.writeHead(200);
+    res.end("hello world\n");
+}
+
+http.createServer(app).listen(80);
+https.createServer(httpsOptions, app).listen(443);
